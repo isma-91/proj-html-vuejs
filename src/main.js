87a08400732911f31110c3ebe10aa73c55a3a945
isma-91 <isma91@hotmail.it>
2022-12-05 15:48:08 +0100

    First Commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue';
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faHatWizard } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
 import App from './App.vue';
 
+library.add(faHatWizard);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 Vue.config.productionTip = false;
 
 new Vue({
